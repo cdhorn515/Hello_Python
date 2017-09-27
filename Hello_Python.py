d8b8eb5502fcb0b45c91dda299ea33d02143e197
Christina Cornell <cdhorn515@gmail.com>
2017-09-26 22:07:37 -0400

    refactored code to put prompt as input param

--- Hello_Python.py
+++ Hello_Python.py
@@ -1,12 +1,7 @@
 print("Hello Python!")
-# end=' ' tells print not to end line with newline character
-print("How old are you?", end=' ')
-age = input()
 
-print("How tall are you in inches?", end=' ')
-height = input()
-
-print("How much do you weigh?", end=' ')
-weight = input()
+age = input("How old are you? ")
+height = input("How tall are you in inches? ")
+weight = input("How much do you weigh? ")
 
 print(f"You're {age} years old, {height} inches tall, and weigh {weight} pounds.")
