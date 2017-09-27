311eebc15c42c6fc794607173b9b6cc7924a33ac
Christina Cornell <cdhorn515@gmail.com>
2017-09-27 12:08:20 -0400

    format and display math result using user input

--- Hello_Python.py
+++ Hello_Python.py
@@ -1,7 +1,11 @@
 print("Hello Python!")
-name = input("What's your name? ")
-age = input(f"Hi {name}, nice to meet you. How old are you? ")
-height = input("How tall are you? ")
-weight = input("How much do you weigh? ")
+# name = input("What's your name? ")
+# age = input(f"Hi {name}, nice to meet you. How old are you? ")
+# height = input("How tall are you? ")
+# weight = input("How much do you weigh? ")
+#
+# print(f"{name} is {age} years old, {height} tall, and weighs {weight} pounds.")
+a = int(input("Enter an integer: "))
+b = int(input("Please enter another integer: "))
 
-print(f"{name} is {age} years old, {height} tall, and weighs {weight} pounds.")
+print("Integer division= {0}\nFloat division= {1}".format(a//b, a/b))
