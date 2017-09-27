58323f73d9154ff1475c998982dd2f7d5c81f306
Christina Cornell <cdhorn515@gmail.com>
2017-09-26 22:20:56 -0400

    used format to add user input to next lines

--- Hello_Python.py
+++ Hello_Python.py
@@ -1,7 +1,7 @@
 print("Hello Python!")
-
-age = input("How old are you? ")
-height = input("How tall are you in inches? ")
+name = input("What's your name? ")
+age = input(f"Hi {name}, nice to meet you. How old are you? ")
+height = input("How tall are you? ")
 weight = input("How much do you weigh? ")
 
-print(f"You're {age} years old, {height} inches tall, and weigh {weight} pounds.")
+print(f"{name} is {age} years old, {height} tall, and weighs {weight} pounds.")
